44d5ab495947d568078e95718c29f9c7e2c9cabf
Gustavo Felipe de Souza <gusttavsouza10@gmail.com>
2025-08-21 08:56:25 -0300

    Primeiro commit de teste pelo NetBeans

--- src/main/java/com/mycompany/sistema/biblioteca/lp2/SistemaBibliotecaLp2.java
+++ src/main/java/com/mycompany/sistema/biblioteca/lp2/SistemaBibliotecaLp2.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sistema.biblioteca.lp2;
-
-/**
- *
- * @author Gustavo
- */
 public class SistemaBibliotecaLp2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Test");
     }
 }
