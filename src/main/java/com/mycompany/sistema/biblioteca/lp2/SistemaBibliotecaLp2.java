e96b0b6061266bc2a62921e88caa99a6c237248f
Gustavo Felupe de Souza <souza.gustavo@aluno.uepb.edu.br>
2025-08-21 09:04:10 -0300

    Primeiro commit de teste pelo NetBeans

--- src/main/java/com/mycompany/sistema/biblioteca/lp2/SistemaBibliotecaLp2.java
+++ src/main/java/com/mycompany/sistema/biblioteca/lp2/SistemaBibliotecaLp2.java
@@ -2,6 +2,6 @@ package com.mycompany.sistema.biblioteca.lp2;
 public class SistemaBibliotecaLp2 {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Ola mundo");
     }
 }
